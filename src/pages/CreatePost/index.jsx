c8052cca1edb7b17f4d213b3b8b921a2ad271cc9
MTahir27 <m.tahiridrees27@gmail.com>
2022-02-11 22:26:25 +0500

    Create post Component Styling

--- src/pages/CreatePost/index.jsx
+++ src/pages/CreatePost/index.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React from 'react';
+import "./style.css";
 
 const CreatePost = () => {
     return (
-        <div>CreatePost</div>
+        <div className="createPostPage">
+            <div className="cpContainer">
+                <h1>Create Post</h1>
+                <div className="inputGp">
+                    <label htmlFor="title">Title</label>
+                    <input type="text" name="title" id="title" placeholder='Title ...' />
+                </div>
+                <div className="inputGp">
+                    <label htmlFor="post">Post</label>
+                    <textarea name="post" id="post" placeholder='Post'></textarea>
+                </div>
+                <button type="submit">Submit Post</button>
+            </div>
+        </div>
     )
 }
 

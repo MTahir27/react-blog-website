a0b68f5f21fa3240e0e957eb18c0670d1abb76c9
MTahir27 <m.tahiridrees27@gmail.com>
2022-02-11 21:50:12 +0500

    Redirect to home page after login

--- src/pages/Signin/index.jsx
+++ src/pages/Signin/index.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import { auth, provider } from "../../Firebase/firebase-config";
 import { signInWithPopup } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
+
 const Signin = ({ setIsAuth }) => {
+    let navigate = useNavigate();
+
     const signInWithGoogle = () => {
         signInWithPopup(auth, provider).then((resut) => {
             localStorage.setItem("isAuth", true);
             setIsAuth(true);
+            navigate("/");
         })
     }
     return (
         <div className='loginpage'>
-            <p>Sign in with Coogle to COntinue</p>
+            <p>Sign in with Coogle to Continue</p>
             <button className='login-with-google-btn' onClick={signInWithGoogle}>Sign in with Google</button>
         </div>
     )

05da32b559b7fa7c2b9e04e88e38b975b64208e5
MTahir27 <m.tahiridrees27@gmail.com>
2022-02-15 10:35:20 +0500

    Login after refresh

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Navbar from "./Components/Navbar";
 import { useState } from "react";
 
 function App() {
-  const [isAuth, setIsAuth] = useState(false);
+  const [isAuth, setIsAuth] = useState(localStorage.getItem("isAuth"));
   return (
     <Router>
       <Navbar isAuth={isAuth} setIsAuth={setIsAuth} />
